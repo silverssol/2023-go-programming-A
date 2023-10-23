a975e9bd93d42fd7ff065a346aa6791786faed9e
eunsol <esol04cc@gmail.com>
2023-10-23 15:02:04 +0900

    1.7

---
+++ week09/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(n1 *int, n2 *int) {
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+}
+
+func main() {
+	a := 10
+	b := 20 // var b int = 20
+
+	// c := &a
+	// var c*int = &a
+	// fmt.Printf("$T\n", c)
+	fmt.Println(a, b)
+	swap(&a, &b) //pass by pointer
+	fmt.Println(a, b)
+}
