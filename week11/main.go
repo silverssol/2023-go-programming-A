0056fa452a153612291e9735d88da9f528894d5a
eunsol <esol04cc@gmail.com>
2023-11-06 14:29:18 +0900

    week11 v2.5

--- week11/main.go
+++ week11/main.go
@@ -13,4 +13,12 @@ func main() {
 	//primes := [3]int{2,3,5} 11번 줄과 똑같음
 	test := [5]bool{true, true, true}
 	fmt.Println(test[3])
+
+	// fmt.Println(primes[4])
+
+	i := 0
+	for i < len(primes) { //panic: runtime error: index out of range [3] with length 3
+		fmt.Println(primes[i])
+		i++
+	}
 }
