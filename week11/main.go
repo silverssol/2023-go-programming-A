cf60da13adf26524b22df0a7ba9644eac5c80ba0
eunsol <esol04cc@gmail.com>
2023-11-06 14:13:50 +0900

    week11 v2.3

---
+++ week11/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 	var primes [3]int
+	// 	primes[0] = 2
+	// 	primes[1] = 3
+	// 	primes[2] = 5
+	// 	fmt.Println(primes, primes[1])
+	var primes [3]int = [3]int{2, 3, 5} //primes 배열을 배열 리터럴로 초기화
+	fmt.Println(primes, primes[1])
+	//primes := [3]int{2,3,5} 11번 줄과 똑같음
+	test := [5]bool{true, true, true}
+	fmt.Println(test[3])
+}
