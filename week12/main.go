d3d21838050b71798723f5c4421c05737aa87008
eunsol <esol04cc@gmail.com>
2023-11-13 14:59:57 +0900

    week12 v 3.3

--- week12/main.go
+++ week12/main.go
@@ -3,29 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// var s []int
-	// s = make([]int, 5)
+	a := []string{"a", "b", "c", "d"}
+	as := a[0:2]
+	as[1] = "z"
+	fmt.Println(a)
+	fmt.Println(as)
 
-	//s := make([]int,5)//make함수를 이용해 슬라이스 생성 후 메모리 할당, 제로 값 적용
-
-	s := []int{0, 0, 0, 0, 0} //슬라이스 리터럴을 이용해 슬라이스 생성 및 메모리 항당, 초기화 진행
-
-	s[4] = 99
-	s[2] = 91
-
-	for _, value := range s {
-		fmt.Println(value)
-	}
-	copys := s[1:4]
-	for _, value := range copys {
-		fmt.Println(value)
-	}
-	test := [3]string{"inha", "go", "student"} //배열 리터럴을 이용해서 test배열 생성
-	//tests:=test[0:4]							//invalid argument: index 4 out of bounds [0:4]
-	tests := test[:2] //tests := test[0:2]
-	tests2 := test[1:]
-	tests2[0] = "python"
-	fmt.Println(tests2)
-	fmt.Println(tests, len(tests))
-	fmt.Println(test)
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
 }
